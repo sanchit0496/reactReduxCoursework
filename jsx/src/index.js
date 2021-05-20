4679fe75c2f90e984920fe13b0cc0d4b52848c2e
Sanchit <sanchit0496@gmail.com>
2021-05-20 12:05:47 +0530

    deleted src files and created Hello World

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import React and ReactDOM library
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+
+const App =  () => {
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App />,
+    document.getElementById('root')
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
+
+
+
+//create a react component
+
+//take the react component and show on screen
