4d69c38fe8550dd62beacf0772e772f4aa082943
Sanchit <sanchit0496@gmail.com>
2021-05-20 13:20:26 +0530

    using global function inside JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,14 +2,20 @@
  import React from 'react';
  import ReactDOM from 'react-dom';
 
+ //add () while calling the function to avoid any error or blank return
+function getButtonText(){
+    return 'Function Click'
+}
+
  //craete component
 const App =  () => {
+    // const buttonText = 'Click Me';
     return(
         <div>
             <label for="name">Name</label>
             <input id="name" type="text" />
             <input type="submit" style = 
-            {{backgroundColor:'lightblue', color: 'white'}} value = "Submit"/>
+            {{backgroundColor:'lightblue', color: 'white'}} value = {getButtonText()}/> 
         </div>
     )
 }
