b2b442b048429a63459e170a3f7a972e368ac65b
Sanchit <sanchit0496@gmail.com>
2021-05-23 23:27:33 +0530

    adding semantic UI and icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
-const SeasonDisplay = props => {
-    console.log(props.lat)
 
-    return(<h1>Hello</h1>)
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9){
+        return lat > 0 ? 'summer' : 'winter';
+    }
+    else{
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth())
+    
+    let text = "";
+    if(season === 'summer'){
+        text = 'Its Summer'
+    }else{
+        text = 'Its Winter'
+    }
+
+    let icon = "";
+    if(season === 'winter'){
+        icon = 'snowflake'
+    }else{
+        icon = 'sun'
+    }
+
+    return(
+        <div>
+            <i className = {`${icon} icon`} />
+            <h1>{text}</h1>
+            <i className = {`${icon} icon`} />
+        </div>
+    )
 }
 
 export default SeasonDisplay;
