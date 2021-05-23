815b9a45c6948ef022fb12639960aed631f1d81e
Sanchit <sanchit0496@gmail.com>
2021-05-23 23:58:21 +0530

    adding spinner and the default props

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner.js';
 
 class App extends React.Component{
 
@@ -25,7 +26,7 @@ class App extends React.Component{
             return <SeasonDisplay lat = {this.state.lat}/>
         }        
         if(!this.state.errorMessage && !this.state.lat){
-            return <div>Loading</div>
+            return <Spinner message = "kindly enter"/>
         }
     }
 
