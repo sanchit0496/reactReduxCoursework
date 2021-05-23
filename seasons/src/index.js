a01abdefc79b710d7529e779ea98bf79a96a2130
Sanchit <sanchit0496@gmail.com>
2021-05-23 18:36:06 +0530

    creating Class

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay.js';
 
-const App = () => {
+class App extends React.Component{
+    render(){
         window.navigator.geolocation.getCurrentPosition(
-                position => console.log(position),
-                err => console.log(err)       
-            )
-        
-    return(<h1>Hello</h1>)
+            position => console.log(position),
+            err => console.log(err)       
+        );
+        return(<h1>Hello</h1>)
+    }
 }
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
