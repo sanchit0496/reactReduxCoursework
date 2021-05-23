53e24f6f0ce94712da85d846bca0bf0f63dc5b19
Sanchit <sanchit0496@gmail.com>
2021-05-23 15:57:27 +0530

    adding buttons

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
-
+import ApprovalCard from './ApprovalCard';
 const App = () => {
     return(
         <div>
-            <CommentDetail name = "Mohit" date = "Two Days Ago" text = "Informative"/>
-            <CommentDetail name = "Myra" date = "Three Days Ago" text = "Knowledgable"/>
-            <CommentDetail name = "Advik" date = "Four Days Ago" text = "Factual"/>
-            <CommentDetail name = "Aaryan" date = "Five Days Ago" text = "Interesting"/>
+            <ApprovalCard>
+                <CommentDetail name = "Mohit" date = "Two Days Ago" text = "Informative"/>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail name = "Myra" date = "Three Days Ago" text = "Knowledgable"/>
+            </ApprovalCard>
+    
+            <ApprovalCard>
+                <CommentDetail name = "Advik" date = "Four Days Ago" text = "Factual"/>
+            </ApprovalCard>
+        
+            <ApprovalCard>
+                <CommentDetail name = "Aaryan" date = "Five Days Ago" text = "Interesting"/>
+            </ApprovalCard>
+
+
         </div>
     )
 }
