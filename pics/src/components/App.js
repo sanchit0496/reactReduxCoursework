d1e53792d7a5f32192bdd7b2ab8526acc4b5ee9a
Sanchit <sanchit0496@gmail.com>
2021-05-24 14:20:36 +0530

    creating user form and  classes

---
+++ pics/src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './SearchBar.js';
+
+class App extends React.Component{
+
+    onSearchSubmit(term){
+        console.log(term);
+    }
+
+    render(){
+        return(
+        <div className = 'ui container' style = {{marginTop: '10px'}}>
+            <SearchBar onSubmit = {this.onSearchSubmit}/>
+        </div>
+        );
+    }
+}
+
+export default App;
