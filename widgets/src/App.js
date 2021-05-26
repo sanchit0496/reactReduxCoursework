401283aac569ea0907160f1051137b5fc1de4b6e
Sanchit <sanchit0496@gmail.com>
2021-05-26 18:11:26 +0530

    adding Dropdown

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
-
-let item = [
-    {
-        title: "title 01",
-        content: "content 01"
-    },
-    {
-        title: "title 02",
-        content: "content 02"
-    },
-    {
-        title: "title 03",
-        content: "content 03"
-    }
-]
-
+import Dropdown from './components/Dropdown';
 
 const App = () => {
     return(
         <div>
-            <Search />
+            <Dropdown />
         </div>
     )
 }
---
+++ widgets/src/components/Dropdown.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dropdown = () => {
+    return(
+        <h1>Hello</h1>
+    )
+}
+
+export default Dropdown;
