5fb3c773970259f335e7e8538f2d3300bb76ede5
Sanchit <sanchit0496@gmail.com>
2021-05-26 20:52:06 +0530

    hide dropdown after selection an option

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+
+    const [open, setOpen] = useState(false);
 
     const renderedOptions = options.map((option) => {
 
@@ -23,10 +25,12 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         <div className="ui form">
           <div className="field">
             <label className="label">Select a Color</label>
-            <div className="ui selection dropdown visible active">
+            <div
+            onClick = { () => setOpen(!open) } 
+            className= {`ui selection dropdown ${open ? 'visible active' : ''}`}>
               <i className="dropdown icon"></i>
               <div className="text">{selected.label}</div>
-              <div className="menu visible transition">{renderedOptions}</div>
+              <div className= {`menu ${open? 'visible transition': ''}`}>{renderedOptions}</div>
             </div>
           </div>
         </div>
