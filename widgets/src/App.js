e40295e1944e931d39c55a0655e0c1f17b94d137
Sanchit <sanchit0496@gmail.com>
2021-05-26 19:29:05 +0530

    different color name on clicking label

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 
+
+let item = [
+    {
+        title: "title 01",
+        content: "content 01"
+    },
+    {
+        title: "title 02",
+        content: "content 02"
+    },
+    {
+        title: "title 03",
+        content: "content 03"
+    }
+]
+
+let options = [
+    {
+        label: 'The Color Red',
+        value: 'Red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'Green'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'Blue'
+    }
+]
+
+
 const App = () => {
+
+    const [selected, setSelected] = useState(options[0]);
+
     return(
         <div>
-            <Dropdown />
+            <Dropdown 
+            selected = {selected}
+            onSelectedChange = {setSelected} 
+            options = {options}/>
         </div>
     )
 }
--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
-const Dropdown = () => {
-    return(
-        <h1>Hello</h1>
-    )
+
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+
+    const renderedOptions = options.map((option) => {
+        return(
+            <div key = {option.key} 
+            className="item"
+            onClick = {() => {onSelectedChange(option)}}
+            >
+                {option.label}
+            </div>
+        )
+    });
+
+    return (
+        <div className="ui form">
+          <div className="field">
+            <label className="label">Select a Color</label>
+            <div className="ui selection dropdown visible active">
+              <i className="dropdown icon"></i>
+              <div className="text">{selected.label}</div>
+              <div className="menu visible transition">{renderedOptions}</div>
+            </div>
+          </div>
+        </div>
+      );
+
 }
+
+
 
 export default Dropdown;
