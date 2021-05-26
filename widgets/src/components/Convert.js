8b553916607731909bc3674a5cb263a41abf0bba
Sanchit <sanchit0496@gmail.com>
2021-05-27 02:07:48 +0530

    translated on the display

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -2,21 +2,32 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
+    const [translated, setTranslated] = useState("");
   useEffect(() => {
-    axios.post(
-      'https://translation.googleapis.com/language/translate/v2',
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
-        },
-      }
-    );
+
+    const doTranslation = async () => {
+        const {data} = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+            params: {
+            q: text,
+            target: language.value,
+            key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+            },
+        }
+        );
+        setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
   }, [language, text]);
 
-  return <div />;
+  return(
+      <div className = 'ui header'>
+          <h1>{translated}</h1>
+      </div>
+  )
 };
 
 export default Convert;
