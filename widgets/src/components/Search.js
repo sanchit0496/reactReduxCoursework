0c14960eb306736c9e08255bff4b857584db465a
Sanchit <sanchit0496@gmail.com>
2021-05-26 16:54:03 +0530

    adding the visit button

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,20 +1,51 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search = () => {
 
-    const [term, setTerm] = useState('');
-
-    console.log("I run with every render");
-
+    const [term, setTerm] = useState('wikipedia');
+    const [results, setResults] = useState([]);
+console.log(results);
     useEffect(() => {
-        console.log("I run at initial render");
-    }, [])
+        const  search =  async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            });
+            setResults(data.query.search);
+        }
+        if(term){
+            search();
+        }
+    }, [term]);
 
 
+    const renderedResults = results.map((result) => {
+        return(
+            <div key = {result.pageid} className="item">
 
-    useEffect(() => {
-        console.log("I run at initial render or when data changed");
-    }, [term])
+                <div className="right floated content">
+                    <a 
+                    href= {`https://en.wikipedia.org?curid=${result.pageid}`} 
+                    className="ui button">
+                        Visit
+                    </a>
+                </div>
+
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML = {{ __html: result.snippet }} ></span>
+                </div>
+            </div>
+        );
+    });
 
     return(
         <div>
@@ -28,6 +59,11 @@ const Search = () => {
                     type="text" />
                 </div>
             </div>
+
+            <div className="ui celled list">
+                {renderedResults}
+            </div>
+
         </div>
     )
 }
