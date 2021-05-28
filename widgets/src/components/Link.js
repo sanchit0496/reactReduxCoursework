c19286199c09299dc805daa3546f0797dd86139b
Sanchit <sanchit0496@gmail.com>
2021-05-28 16:52:11 +0530

    rendering component on screen

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -5,6 +5,10 @@ const Link = ({className, href, children}) => {
     const onClick = (event) => {
         event.preventDefault();
         window.history.pushState({}, "", href);
+
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
+
     }
 
     return (
--- widgets/src/components/Route.js
+++ widgets/src/components/Route.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Route = ({ path, children }) =>{
+
+    const [currentPath, setCurrentPath] = useState(window.location.pathname)
+    useEffect(() => {
+        
+        const onLocationChange = () => {
+            setCurrentPath(window.location.pathname)
+        }
+        window.addEventListener('popstate', onLocationChange);
+
+        return () => {
+            window.addEventListener('popstate', onLocationChange);
+        }
+    }, [])
+
     if(window.location.pathname === path){
         return children;
     }else{
