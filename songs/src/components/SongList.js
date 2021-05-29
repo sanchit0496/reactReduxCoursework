d1f2f4205107f7af9230d300540732b45f1ac0ff
Sanchit <sanchit0496@gmail.com>
2021-05-29 16:22:18 +0530

    selected song into the console

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import  { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends React.Component{
-    render(){
-        console.log(this.props);
-        return <div>SongList</div>
+
+    renderList(){
+        return this.props.songs.map((song) => {
+            return(
+                <div key = {song.title} className="item">
+                    <div className="right floated content">
+                        <button
+                        onClick = {() => this.props.selectSong(song)} 
+                        className="ui button primary">
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">
+                        {song.title}
+                    </div>
+                </div>
+            );
+        });
     }
+
+    render(){
+        return <div className = 'ui divided list'>{this.renderList()}</div>
+    }
+
 }
 
 const mapStateToProp = (state) => {
+    console.log(state);
     return{songs: state.songs};
 } 
 
-export default connect(mapStateToProp)(SongList)
+export default connect(mapStateToProp, {selectSong: selectSong})(SongList)
