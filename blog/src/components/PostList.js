f43094e8c9a867f73aa865796f3a27ab361909ea
Sanchit <sanchit0496@gmail.com>
2021-05-31 12:54:37 +0530

    connecting

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions';
 
 class PostList extends React.Component{
+
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
+
     render(){
         return(
             <div>
-                Post List
+                <PostList />
             </div>
         )
     }
 }
 
 
-export default PostList;
+export default connect(null, {fetchPosts})(PostList);
